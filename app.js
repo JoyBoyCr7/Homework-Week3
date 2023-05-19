b44e33c72555ae3fd4177589a2bdd9497cc35e51
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-05-19 00:04:11 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 $(() => {
 	$("#container")
-    const $h1 = $("<h1></h1>").text("Hogwarts")
+    let $h1 = $("<h1></h1>").text("Hogwarts")
     $("body").append($h1)
+    let $h2 = $("<h2>Ronard Nyongkah</h2>")
+    let $h3 = $("<h3>Slytherin</h3>")
+    let $h4 = $("<h4>AntMan</h4>").attr("class","dog")
+    let $wand = $("<h4>Ronard's wand is wandith</h4>")
+    $("body").append($h2)
+    $("body").append($h3)
+    $("body").append($h4)
+    $("body").append($wand)
 });
 
 
